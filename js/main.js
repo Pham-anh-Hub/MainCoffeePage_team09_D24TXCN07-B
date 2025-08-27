d716d6eedbc858409847378750dcd9f104e63695
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2025-08-27 21:46:19 +0700

    update logic

--- js/main.js
+++ js/main.js
@@ -1,70 +1,39 @@
-(function ($) {
-    "use strict";
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
+// Chờ khi trang web tải xong
+document.addEventListener("DOMContentLoaded", function () {
+
+    // 1. Dropdown menu (chỉ hoạt động khi màn hình rộng > 992px)
+    function setupDropdown() {
+        let dropdowns = document.querySelectorAll(".navbar .dropdown");
+        if (window.innerWidth > 992) {
+            dropdowns.forEach(function (dropdown) {
+                dropdown.addEventListener("mouseenter", function () {
+                    this.querySelector(".dropdown-toggle").click();
                 });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+                dropdown.addEventListener("mouseleave", function () {
+                    this.querySelector(".dropdown-toggle").click();
+                });
+            });
         } else {
-            $('.back-to-top').fadeOut('slow');
+            dropdowns.forEach(function (dropdown) {
+                dropdown.onmouseenter = null;
+                dropdown.onmouseleave = null;
+            });
+        }
+    }
+    setupDropdown();
+    window.addEventListener("resize", setupDropdown);
+
+    // 2. Nút "Back to top"
+    let backToTopBtn = document.querySelector(".back-to-top");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
-
-    // Date and time picker
-    $('.date').datetimepicker({
-        format: 'L'
-    });
-    $('.time').datetimepicker({
-        format: 'LT'
+    backToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
     });
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-})(jQuery);
-
+});
