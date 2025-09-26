812bcc1487a79fd91f207ac3722793dfb8480b0f
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2025-09-26 20:08:35 +0700

    update

---
+++ js/bookings.js
@@ -0,0 +1,140 @@
+const tableListBooking = document.querySelector("#list-bookings")
+const BtnPageElement = document.querySelector("#numberPageElement");
+const btnPrevPage = document.querySelector("#PrevBtnBox");
+const btnNextPage = document.querySelector("#NextBtnBox");
+const addProjectBtn = document.querySelector("#addProjectBtn");
+
+const listBooking = JSON.parse(localStorage.getItem("bookings")) || [];
+
+// Hàm renderPage
+let currentPage = 1; // Mặc định trang đầu tiên là 1
+// totalPerPage
+const totalPerPage = 5;
+let totalPages;
+let start = 0; // Biến cho vị trí bắt đầu
+let end = 4; // Biến cho vị trí kết thúc
+// Mặc định giá trị bắt đầu vfa kết thúc cho danh sách trang đầu tiên
+
+
+function renderBookingList() {
+    tableListBooking.innerHTML = "";
+    // Lọc ra các dự án mà người đang đăng nhập tạo ra
+    const filterBooking = listBooking
+
+    start = (currentPage - 1) * totalPerPage;
+    end = currentPage * totalPerPage;
+
+    if (end >= filterBooking.length) {
+        end = filterBooking.length; // Nếu vị trí kết thúc theo công thức lớn hơn tổng số dự án trong mảng thì gán lại cho end bằng listProject.length luôn
+    }
+    for (let i = start; i < end; i++) {
+        const booking = filterBooking[i];
+        let actionHTML = "";
+
+        if (booking.status === "pending" || !booking.status) {
+            actionHTML = `
+            <button id="editBtn" onclick="cancelBookingInfor('${booking.email}')">Hủy bàn</button>
+            <button id="deleteBtn" onclick="doneBooking('${booking.email}')">Hoàn thành</button>
+        `;
+        } else if (booking.status === "done") {
+            actionHTML = `<span style="color: green; font-weight: bold;">Đã hoàn thành</span>`;
+        } else if (booking.status === "cancelled") {
+            actionHTML = `<span style="color: red; font-weight: bold;">Đã hủy</span>`;
+        }
+
+        tableListBooking.innerHTML += `
+        <tr>
+            <td id="idBooking">${i + 1}</td>
+            <td id="bookingClient">${booking.name}</td>
+            <td id="bookingEmailClient">${booking.email}</td>
+            <td id="bookingTime">${booking.time}</td>
+            <td id="reserveTime">${booking.date}</td>
+            <td id="actionToBooking">${actionHTML}</td>
+        </tr>`;
+    }
+    console.log("tableProjectList: ", tableListBooking);
+}
+
+function renderPage() {
+    console.log("renderPage");
+
+    const filterBooking = listBooking
+    // reset lại danh sách nút trang --> không bị lặp lại
+    BtnPageElement.innerHTML = "";
+
+    // total record - tổng số lượng project = listProject.length
+    totalPages = Math.ceil(filterBooking.length / totalPerPage);
+    for (let i = 1; i <= totalPages; i++) {
+        console.log("Nút số: ", i);
+
+        //tạo ra element Button cho mỗi lần lặp
+        const btnPage = document.createElement("button");
+        console.log("btnPage: ", btnPage);
+
+        btnPage.classList.add("buttonChild");
+        // Gán tiêu đề cho button
+        btnPage.textContent = i;
+
+        // Kiểm tra xem trang nào đang được chọn --> thì active cho btn trang đó
+        if (currentPage === i) {
+            btnPage.classList.add("buttonChild-Active");
+        }
+        //Kiểm tra điều kiện cho nút Next và disabled
+        if (currentPage === totalPages) {
+            btnNextPage.setAttribute("disabled", true);
+        } else {
+            btnNextPage.removeAttribute("disabled");
+        }
+
+        // Tương tự kiểm tra điều kiện cho nút Prev và disable
+        if (currentPage === 1) {
+            btnPrevPage.setAttribute("disabled", true);
+        } else {
+            btnPrevPage.removeAttribute("disabled");
+        }
+
+        // tạo event click cho button đại diện cho trang
+        btnPage.addEventListener("click", function () {
+            // Khi click vào từng button thì ==> gán lại giá trị cho currentPage  = giá trị mà nút bấm vào
+            currentPage = i;
+            console.log("currentPage: ", currentPage);
+            renderPage(); // render lại số trang
+            renderBookingList(filterBooking);
+        });
+        BtnPageElement.appendChild(btnPage);
+    }
+    // start = (currentPage - 1) * totalPerPage;
+    // end = currentPage * totalPerPage;
+}
+
+renderBookingList()
+renderPage()
+
+
+const cancelBookingInfor = (email) => {
+    if (confirm("Bạn có chắc chắn muốn HỦY bàn này không?")) {
+        const index = listBooking.findIndex(b => b.email === email);
+        if (index !== -1) {
+            listBooking[index].status = "cancelled";
+
+            localStorage.setItem("bookings", JSON.stringify(listBooking));
+
+            renderBookingList();
+            renderPage();
+        }
+    }
+};
+
+const doneBooking = (email) => {
+    if (confirm("Bàn được đặt đã hoàn thành ?")) {
+        const index = listBooking.findIndex(b => b.email === email);
+        if (index !== -1) {
+            listBooking[index].status = "done";
+
+            localStorage.setItem("bookings", JSON.stringify(listBooking));
+
+            renderBookingList();
+            renderPage();
+        }
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+const accountLocal = JSON.parse(localStorage.getItem("accounts")) || [];
 // Chờ khi trang web tải xong
 document.addEventListener("DOMContentLoaded", function () {
-
     // 1. Dropdown menu (chỉ hoạt động khi màn hình rộng > 992px)
     function setupDropdown() {
         let dropdowns = document.querySelectorAll(".navbar .dropdown");
@@ -32,8 +32,47 @@ document.addEventListener("DOMContentLoaded", function () {
             backToTopBtn.style.display = "none";
         }
     });
-    backToTopBtn.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-
+    if (backToTopBtn) {
+        backToTopBtn.addEventListener("click", function () {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    }
 });
+
+if (document.querySelector("#registerBtn")) {
+    document
+        .querySelector("#registerBtn")
+        .addEventListener("click", function (e) {
+            e.preventDefault();
+            if (
+                document.querySelector("#registerUsername").value.includes("@") &&
+                document.querySelector("#registerUsername").value.endsWith(".com") &&
+                document.querySelector("#registerPassword").value ==
+                document.querySelector("#confirmPassword").value
+            ) {
+                const newUser = {
+                    email: document.querySelector("#registerUsername").value,
+                    password: document.querySelector("#registerPassword").value,
+                };
+                accountLocal.push(newUser);
+                localStorage.setItem("accounts", JSON.stringify(accountLocal));
+                window.location.href = "./login.html";
+            }
+        });
+}
+if (document.querySelector("#loginBtn")) {
+    document.querySelector("#loginBtn").addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log("safs");
+
+        if (
+            accountLocal.find(
+                (account) =>
+                    account.email == document.querySelector("#loginUsername").value &&
+                    account.password == document.querySelector("#loginPassword").value
+            )
+        ) {
+            window.location.href = "./bookings.html";
+        }
+    });
+}
